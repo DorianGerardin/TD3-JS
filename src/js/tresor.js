eea98c6beaa1684caccde52e57bd54f950817eea
DorianGerardin <gryfadon@gmail.com>
2020-02-20 15:38:47 +0100

    fin ex 2 / class tresor done

--- src/js/tresor.js
+++ src/js/tresor.js
@@ -1,11 +1,11 @@
 class Tresor extends Element {
 
 	constructor(x) {
-		// à compléter
+		super(x, 1, "tresor");
 	}
 
 	initialiser(x) {
-		// à compléter
+		super.initialiser(x, 1, "img/tresor.png");
 	}
 
 }
