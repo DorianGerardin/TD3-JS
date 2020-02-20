1fe148bb98fbc0aedd4f18ca5e56544e02f2c414
DorianGerardin <gryfadon@gmail.com>
2020-02-20 15:00:36 +0100

    class element.js done

--- src/js/element.js
+++ src/js/element.js
@@ -1,19 +1,25 @@
 class Element {
 
 	constructor(x,y,id) {
-		// à compléter
+		this.coordX = x;
+		this.coordY = y;
+		this.sprite = document.getElementById(id);
 	}
 
 	initialiser(x,y,str) {
-		// à compléter
+		this.coordX = x;
+		this.coordY = y;
+		this.setSrc(str);
+		this.placer();
 	}
 
 	setSrc(str) {
-		// à compléter
+		this.sprite.src = str;
 	}
 
 	placer() {
-		// à compléter
+		this.sprite.style.top = 51 + 20 * (this.coordY - 1) + "px";
+		this.sprite.style.left = 51 + 20 * (this.coordX - 1) + "px";
 	}
 
 }
